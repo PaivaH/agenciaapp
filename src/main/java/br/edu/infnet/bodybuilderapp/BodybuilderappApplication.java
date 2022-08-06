df58b37e91981461b636f5009c27e26011d007f8
Henio Paiva <henio_paiva@hotmail.com>
2022-08-06 17:17:14 -0300

    Commit inicial

---
+++ src/main/java/br/edu/infnet/bodybuilderapp/BodybuilderappApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.bodybuilderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodybuilderappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodybuilderappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/bodybuilderapp/BodybuilderappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.bodybuilderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodybuilderappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
